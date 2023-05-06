fa1930dc75ad22e80af6fc7fd2c599752b696614
viethai23 <102601378+viethai23@users.noreply.github.com>
2023-05-06 22:42:28 +0700

    backend spring boot

---
+++ BE/src/main/java/com/btl_web/btl_web/BtlWebApplication.java
@@ -0,0 +1,13 @@
+package com.btl_web.btl_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtlWebApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/btl_web/btl_web/BtlWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btl_web.btl_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
